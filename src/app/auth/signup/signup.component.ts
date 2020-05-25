c2084a9b5a10a58958390fe1db24f0d36e00af21
wilson-kao <wilsonkao@xrex.io>
2020-05-25 16:28:30 +0800

    add date picker

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -8,10 +8,14 @@ import { NgForm } from '@angular/forms';
 })
 export class SignupComponent implements OnInit {
     email = '123@mail.com';
-
+    password = '';
+    maxDate: Date;
     constructor() {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+      this.maxDate = new Date();
+      this.maxDate.setDate(this.maxDate.getFullYear() - 18 );
+    }
 
     onSubmit(form: NgForm){
       console.log(form);
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,9 +3,27 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
-    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule]
+    imports: [
+        MatButtonModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatRippleModule
+    ],
+    exports: [
+        MatButtonModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatRippleModule
+    ]
 })
 export class MaterialModule {}
